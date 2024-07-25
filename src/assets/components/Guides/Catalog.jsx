cc1660fafda788777fd27484cf099b5d8d9c4f6b
i-am-tian <sebastian.alvarezla@campusucc.edu.co>
2024-07-24 21:29:36 -0500

    descarga de guias activa

--- src/assets/components/Guides/Catalog.jsx
+++ src/assets/components/Guides/Catalog.jsx
@@ -2,11 +2,18 @@
 import React from 'react';
 
 import storytellingImage from '/story.png';
+import transformacion from '/transformacion.png';
 import mapsImage from '/maps.png';
 import redImage from '/redssapp.png';
 import cadena from '/cadena.png';
+import virtualvent from '/virtualvent.png';
+import cadenaUrl from '/cadenasuministro.pdf';
+import virtualventUrl from '/guiavirtualvent.pdf';
+import transforUrl from '/transformaciondigital.pdf';
+import mapsUrl from '/googlemaps.pdf';
+import redsUrl from '/redssappguia.pdf';
+import storyUrl from '/storytelling.pdf';
 import '../../css/catalog.css';
-import DownloadForm from './DownloadForm';
 
 const Catalog = () => {
   return (
@@ -15,36 +22,34 @@ const Catalog = () => {
         <h1>¡Aprende con TransDigitalCoop!</h1>
         <div className="cards">
           <div className="card">
-            <img
-              src={mapsImage}
-              alt="Cadena de suministro 4.0"
-              className="card-image"
-            />
-            <h2>Transformación Digital</h2>
-            <p>
-              ¿Qué es la transformación digital? Impulsa tu empresa digitalmente
-            </p>
-            <DownloadForm pdfUrl="/path/to/transformacion_digital.pdf" title="Transformación Digital" />
-          </div>
-          <div className="card">
             <img src={mapsImage} alt="MAPS" className="card-image" />
             <h2>Maps</h2>
             <p>
               Guía Completa para Empresas: Maximiza tu Presencia en Google Maps
             </p>
-            <DownloadForm pdfUrl="/path/to/maps.pdf" title="Maps" />
+            <a href={mapsUrl} download>Descargar</a>
+          </div>
+          <div className="card">
+            <img
+              src={storytellingImage}
+              alt="Storytelling"
+              className="card-image"
+            />
+            <h2>Storytelling</h2>
+            <p>Guión para grabar tu Storytelling Vol. 2</p>
+            <a href={storyUrl} download>Descargar</a>
           </div>
           <div className="card">
             <img
               src={redImage}
-              alt="Cadena de suministro 4.0"
+              alt="RedssApp"
               className="card-image"
             />
             <h2>RedssApp</h2>
             <p>
               ¿Quieres formar parte de nuestra red de asociatividad empresarial? Aprende cómo.
             </p>
-            <DownloadForm pdfUrl="/path/to/redssapp.pdf" title="RedssApp" />
+            <a href={redsUrl} download>Descargar</a>
           </div>
           <div className="card">
             <img
@@ -56,29 +61,31 @@ const Catalog = () => {
             <p>
               ¿Qué es la cadena de suministro 4.0? Aprende ahora
             </p>
-            <DownloadForm pdfUrl="/cadenasuministro.pdf" title="Cadena de suministro 4.0" />
+            <a href={cadenaUrl} download>Descargar</a>
           </div>
           <div className="card">
             <img
-              src={storytellingImage}
-              alt="Storytelling"
+              src={transformacion}
+              alt="Transformación Digital"
               className="card-image"
             />
-            <h2>Storytelling</h2>
-            <p>Guión para grabar tu Storytelling Vol. 2</p>
-            <DownloadForm pdfUrl="/path/to/storytelling.pdf" title="Storytelling" />
+            <h2>Transformación Digital</h2>
+            <p>
+              ¿Qué es la transformación digital? Impulsa tu empresa digitalmente
+            </p>
+            <a href={transforUrl} download>Descargar</a>
           </div>
           <div className="card">
             <img
-              src={mapsImage}
-              alt="Cadena de suministro 4.0"
+              src={virtualvent}
+              alt="Storytelling"
               className="card-image"
             />
             <h2>Comercio Electrónico</h2>
             <p>
               ¿Qué es el comercio electrónico? Mejora tu presencia digital.
             </p>
-            <DownloadForm pdfUrl="/path/to/comercio_electronico.pdf" title="Comercio Electrónico" />
+            <a href={virtualventUrl} download>Descargar</a>
           </div>
         </div>
       </div>
