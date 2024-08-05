fdf17cd33364efc9c0cc1b266dfabb873827b168
i-am-tian <sebastian.alvarezla@campusucc.edu.co>
2024-08-05 15:01:37 -0500

    video desde youtube

--- src/assets/components/Home/Video.jsx
+++ src/assets/components/Home/Video.jsx
@@ -6,7 +6,9 @@ const Video = () => {
   return (
     <div className="video-container">
       <h2>¿Quienes somos?</h2>
-      <video src={video} controls />
+      <video src={video} controls>
+        <source src="https://youtu.be/JdF8NPlXpKI" type="video/mp4"/>
+      </video>
       <a href="/nosotros">Conoce más aquí</a>
     </div>
   )
