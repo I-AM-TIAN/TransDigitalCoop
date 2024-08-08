5105dda339563325bfc4c386bbc1e8b7f2a1dd84
i-am-tian <sebastian.alvarezla@campusucc.edu.co>
2024-08-08 14:41:52 -0500

    cambio url del video

--- src/assets/components/Home/Video.jsx
+++ src/assets/components/Home/Video.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import video from '/video_transdigitalcoop.mp4';
-import '../../css/video.css';
+import React from "react";
+import "../../css/video.css";
 
 const Video = () => {
   return (
     <div className="video-container">
       <h2>¿Quienes somos?</h2>
       <iframe
-        src="https://www.youtube.com/embed/JdF8NPlXpKI"
-        title="TransDigitalCoop Video"
+        src="https://www.youtube.com/embed/lpiAgSoD-P8?si=RI39FYG__6_ee_V5"
+        title="YouTube video player"
         frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      />
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen="true"
+      ></iframe>
       <a href="/nosotros">Conoce más aquí</a>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
