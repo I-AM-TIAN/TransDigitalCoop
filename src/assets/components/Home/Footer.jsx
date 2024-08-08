12dcf311755d26371ecdd4ab1c0b5e957a98fa5c
i-am-tian <sebastian.alvarezla@campusucc.edu.co>
2024-08-08 16:03:02 -0500

    facebook y asistente virtual

--- src/assets/components/Home/Footer.jsx
+++ src/assets/components/Home/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <p>© 2024 TransDigitalCoop. Todos los derechos reservados.</p>
       </div>
       <div className="social-icons">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/share/jt8rQFFAVDqqgUML/?mibextid=qi2Omg" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-facebook-f"></i>
         </a>
         <a href="https://www.youtube.com/channel/UC8pUqx0B6hlhcfHdoDNb2LA" target="_blank" rel="noopener noreferrer">
