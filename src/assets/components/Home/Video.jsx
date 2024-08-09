b1599f519814bb081b448b35b98438afd5c07075
i-am-tian <sebastian.alvarezla@campusucc.edu.co>
2024-08-09 10:45:24 -0500

    optimizacion de video

--- src/assets/components/Home/Video.jsx
+++ src/assets/components/Home/Video.jsx
@@ -1,18 +1,14 @@
 import React from "react";
+import "@justinribeiro/lite-youtube";
 import "../../css/video.css";
 
 const Video = () => {
   return (
     <div className="video-container">
       <h2>¿Quienes somos?</h2>
-      <iframe
-        src="https://www.youtube.com/embed/lpiAgSoD-P8?si=RI39FYG__6_ee_V5"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen="true"
-      ></iframe>
+      <div className="video-reproductor">
+        <lite-youtube videoid="lpiAgSoD-P8"></lite-youtube>
+      </div>
       <a href="/nosotros">Conoce más aquí</a>
     </div>
   );
